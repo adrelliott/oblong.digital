5b4addde3daf006ab36fe4cfaa824701c969f9b7
= <=>
2020-05-21 14:35:09 +0100

    Adjust image quality

--- partials/navbar-full.php
+++ partials/navbar-full.php
@@ -1,10 +1,10 @@
 <!-- Start of Full Nav -->
         <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">
-                    <img src="/assets/img/oblong_logo_v6_400px.png" width="300px" />
+            <div class="container ">
+                <a class="navbar-brand js-scroll-trigger align-content-xs-center align-content-sm-center align-content-md-center" href="#page-top">
+                    <img src="/assets/img/oblong_logo_v6_275px.png" class="" />
                 </a>
-                <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded text-sm-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu <i class="fas fa-bars"></i></button>
+                <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded mt-2" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu <i class="fas fa-bars"></i></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#skills">Skills</a></li>
